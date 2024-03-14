edd3cf1abe146b337d8e00fe99034d310fa3e467
shinjaesang <tlswltkd1011@gmail.com>
2024-03-14 16:24:01 +0900

    When 사용

--- app/src/main/java/kr/ac/buttonevent0314/MainActivity.kt
+++ app/src/main/java/kr/ac/buttonevent0314/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
@@ -13,34 +14,51 @@ class MainActivity : AppCompatActivity() {
     lateinit var btn911 : Button
     lateinit var btnGallery : Button
     lateinit var btnFinish : Button
+    lateinit var btnListener : View.OnClickListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn = findViewById(R.id.button)
         btnHome = findViewById(R.id.buttonHome)
-        btnHome = findViewById(R.id.button911)
-        btnHome = findViewById(R.id.buttonGallery)
-        btnHome = findViewById(R.id.buttonFinsh)
+        btn911 = findViewById(R.id.button911)
+        btnGallery = findViewById(R.id.buttonGallery)
+        btnFinish = findViewById(R.id.buttonFinsh)
+
+        btnListener = View.OnClickListener {
+            when(it.id){
+                R.id.buttonHome -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kopo.ac.kr/jungsu/index.do"))
+                R.id.button911 -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"))
+                R.id.buttonGallery -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"))
+            }
+            startActivity(intent)
+        }
+
+        btnHome.setOnClickListener(btnListener)
+        btn911.setOnClickListener(btnListener)
+        btnGallery.setOnClickListener(btnListener)
+
+        btn.setOnClickListener {
+            Toast.makeText(applicationContext, "기분 좋은 오후입니다.", Toast.LENGTH_SHORT).show()
+        }
 
         btnHome.setOnClickListener {
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(("https://www.kopo.ac.kr/jungsu/index.do")))
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kopo.ac.kr/jungsu/index.do"))
             startActivity(intent)
         }
 
         btn911.setOnClickListener {
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(("tel:/911")))
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"))
             startActivity(intent)
         }
+
         btnGallery.setOnClickListener {
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(("content://media/internal/images/media")))
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"))
             startActivity(intent)
         }
+
         btnFinish.setOnClickListener {
-           finish()
-        }
-        btn.setOnClickListener {
-            Toast.makeText(applicationContext, "기분 좋은 오후입니다.",  Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
-}
+}}
